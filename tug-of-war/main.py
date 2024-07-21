51bd82121ac028144a1752030e042d1d690c3a69
Wei Ren <renwei2004@gmail.com>
2024-07-20 17:47:27 -0700

    Change the motor deg from integer to float
    The increment of 2 degrees in the interactive debug mode is too big in the execution mode. Reduce it to 0.02 so that it will take ~ 9 moves to reach the winning condition.
    
    Change-Id: I109a0bcb30d523cb3b2c87c61160b67b30a29814

--- tug-of-war/main.py
+++ tug-of-war/main.py
@@ -8,22 +8,22 @@ robot = RobotBit()
 
 deg = 65
 start = 1
-robot.geekServo9g(1, int(deg))
-screen.fill((170, 0, 0))
+robot.geekServo9g(1, deg)
+screen.fill((255, 193, 204))
 screen.text('Tug\nof\nWar',26,45,2,(255, 255, 255))
 while not start == 0:
   if sensor.btnValue('left'):
-    deg += -3
+    deg += -0.02
   if sensor.btnValue('a'):
-    deg += 3
+    deg += 0.02
   if deg < 40 or deg > 90:
     start = 0
   else:
-    robot.geekServo9g(1, int(deg))
+    robot.geekServo9g(1, deg)
 buzzer.tone(440, 1)
 if deg < 40:
-  screen.fill((0, 0, 170))
+  screen.fill((255, 95, 150))
   screen.text('Left wins!',26,45,3,(255, 255, 255))
 else:
-  screen.fill((170, 0, 0))
+  screen.fill((220, 150, 207))
   screen.text('Right wins!',26,45,3,(255, 255, 255))
