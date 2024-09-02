24510af74ccc7e2ddfc91a52fcdde87a209d1a12
Wei Ren <renwei2004@gmail.com>
2024-09-02 11:13:09 -0700

    Display the open/closed status of the trash can on the screen
    Also fix the bug that the trash can does not auto close after
    5 seconds it is opened.
    
    Note that the ImportError of module `sonar` can be fixed by
    refreshing the firmware.
    
    Change-Id: Id87255b2f72106a6167d1c7ec3c04060a79ca614
    Signed-off-by: Wei Ren <renwei2004@gmail.com>

--- smart-trash-can/smart-trash-can.py
+++ smart-trash-can/smart-trash-can.py
@@ -5,21 +5,27 @@ import time
 
 close_angle = 0
 open_angle = 0
+open_time = 0
 dis = 0
-old_time = 0
 meowSonar_P1 = MeowSonar('P1')
 robot = RobotBit()
 
 
 close_angle = 80
-open_angle = 20
+open_angle = 30
+open_time = -1
 robot.geekServo9g(1, close_angle)
 screen.fill(0)
-screen.text('Trash Can',10,55,2,(0, 170, 170))
+screen.text('Trash Can\nClosed',10,55,2,(0, 170, 170))
 while True:
   dis = meowSonar_P1.checkdist()
   if dis < 10 and dis > 1:
     robot.geekServo9g(1, open_angle)
-    old_time = time.ticks_ms()
-  if old_time - time.ticks_ms() > 5000:
+    screen.fill(0)
+    screen.text('Trash Can\nOpen',10,55,2,(170, 0, 0))
+    open_time = time.ticks_ms()
+  if open_time > 0 and time.ticks_ms() - open_time > 5000:
     robot.geekServo9g(1, close_angle)
+    open_time = -1
+    screen.fill(0)
+    screen.text('Trash Can\nClosed',10,55,2,(0, 170, 170))
